c0a87d99d6c6223222ee65bd1ff04346f0954e98
ChallengerCY <790597004@qq.com>
2019-05-26 21:52:55 +0800

    Signed-off-by: ChallengerCY <790597004@qq.com>

---
+++ TestAllAPI/Assets/UnityEngine/Classes/Mathf/Script/TestMathClamp.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMathClamp : MonoBehaviour
+{
+    private float xMin = -1.0f, xMax = 1.0f;
+    private float timeValue = 0.0f;
+
+    public int health = 17;
+    private int[] healthUp = new int[] { 25, 10, 5, 1 };
+    private int[] healthDown = new int[] { -10, -5, -2, -1 };
+
+    // Width and height for the buttons.
+    private int xButton = 75;
+    private int yButton = 50;
+
+    // Place of the top left button.
+    private int xPos1 = 50, yPos1 = 100;
+    private int xPos2 = 125, yPos2 = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        // Compute the sin position.
+        float xValue = Mathf.Sin(timeValue * 5.0f);
+
+        // Now compute the Clamp value.
+        float xPos = Mathf.Clamp(xValue, xMin, xMax);
+
+        // Update the position of the cube.
+        transform.position = new Vector3(xPos, 0.0f, 0.0f);
+
+        // Increase animation time.
+        timeValue = timeValue + Time.deltaTime;
+
+        // Reset the animation time if it is greater than the planned time.
+        if (xValue > Mathf.PI * 2.0f)
+        {
+            timeValue = 0.0f;
+        }
+    }
+
+    void OnGUI()
+    {
+        // Let the minimum and maximum values be changed
+        xMin = GUI.HorizontalSlider(new Rect(25, 25, 100, 30), xMin, -1.0f, +1.0f);
+        xMax = GUI.HorizontalSlider(new Rect(25, 60, 100, 30), xMax, -1.0f, +1.0f);
+
+        // xMin is kept less-than or equal to xMax.
+        if (xMin > xMax)
+        {
+            xMin = xMax;
+        }
+
+        // Display the xMin and xMax value with better size labels.
+        GUIStyle fontSize = new GUIStyle(GUI.skin.GetStyle("label"));
+        fontSize.fontSize = 24;
+
+        GUI.Label(new Rect(135, 10, 150, 30), "xMin: " + xMin.ToString("f2"), fontSize);
+        GUI.Label(new Rect(135, 45, 150, 30), "xMax: " + xMax.ToString("f2"), fontSize);
+
+
+
+        GUI.skin.button.fontSize = 20;
+
+        // Generate and show positive buttons.
+        for (int i = 0; i < healthUp.Length; i++)
+        {
+            if (GUI.Button(new Rect(xPos1, yPos1 + i * yButton, xButton, yButton), healthUp[i].ToString()))
+            {
+                health += healthUp[i];
+            }
+        }
+
+        // Generate and show negative buttons.
+        for (int i = 0; i < healthDown.Length; i++)
+        {
+            if (GUI.Button(new Rect(xPos2, yPos2 + i * yButton, xButton, yButton), healthDown[i].ToString()))
+            {
+                health += healthDown[i];
+            }
+        }
+
+        // Show health between 1 and 100.
+        health = Mathf.Clamp(health, 1, 100);
+        GUI.Label(new Rect(xPos1, xPos1, 2 * xButton, yButton), "Health: " + health.ToString("D3"));
+    }
+}
---
+++ TestAllAPI/Assets/UnityEngine/Classes/Mathf/Script/TestMathf.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMathf : MonoBehaviour
+{
+    public float deg = 30.0F;
+
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float rad = deg * Mathf.Deg2Rad;
+        Debug.Log(deg + " degrees are equal to " + rad + " radians.");
+
+        Debug.Log("Mathf.Epsilon is" + Mathf.Epsilon);
+
+        Debug.Log("Mathf.Infinity is" + Mathf.Infinity);
+
+        Debug.Log("Mathf.NegativeInfinity is" + Mathf.NegativeInfinity);
+
+        Debug.Log("Mathf.PI is" + Mathf.PI);
+
+        rad = 10.0f;
+        float deg1 = rad * Mathf.Rad2Deg;
+        Debug.Log(rad + " radians are equal to " + deg1 + " degrees.");
+
+        Debug.Log("-1 的 绝对值是:" + Mathf.Abs(-1));
+
+        Debug.Log("0.5f的反余弦值是:" + Mathf.Acos(0.5f));
+
+        Debug.Log("1f = 1f ？" + Mathf.Approximately(1F, 1F));
+
+        Debug.Log("0.5f的反正弦值是:" + Mathf.Asin(0.5f));
+
+        Debug.Log("0.5f的反正切值是:" + Mathf.Atan(0.5f));
+
+        Example();
+    }
+
+
+    /// <summary>
+    /// 判断两个浮点数是否相等
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    bool isEqual(float a, float b)
+    {
+        if (a >= b - Mathf.Epsilon && a <= b + Mathf.Epsilon)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void Update()
+    {
+        // shows the line that follows the ray.
+        Debug.DrawLine(Vector3.zero, Vector3.forward * 100);
+        if (Physics.Raycast(Vector3.zero, Vector3.forward, Mathf.Infinity))
+        {
+            print("There is something in front of the object!");
+        }
+
+        Vector3 relative = transform.InverseTransformPoint(target.position);
+        float angle = Mathf.Atan2(relative.x, relative.z) * Mathf.Rad2Deg;
+        transform.Rotate(0, angle, 0);
+
+      //Debug.Log(Mathf.Clamp(Time.time, 0, 1))  ;
+    }
+
+    void Example()
+    {
+        // Prints 10
+        Debug.Log(Mathf.Ceil(10.0F));
+        // Prints 11
+        Debug.Log(Mathf.Ceil(10.2F));
+        // Prints 11
+        Debug.Log(Mathf.Ceil(10.7F));
+        // Prints -10
+        Debug.Log(Mathf.Ceil(-10.0F));
+        // Prints -10
+        Debug.Log(Mathf.Ceil(-10.2F));
+        // Prints -10
+        Debug.Log(Mathf.Ceil(-10.7F));
+    }
+}
